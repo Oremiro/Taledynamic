76cf481e03bb9ee7fbcd30a235eab9f2adbff9e8
Creator148 <bak-home@mail.ru>
2021-10-10 13:46:29 +0300

    Add telegram.bot

---
+++ Taledynamic.Bot/Configuration.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TaleDynamicBot
+{
+    public static class Configuration
+    {
+        public readonly static string BotToken = Environment.GetEnvironmentVariable("botoken");
+    }
+}
---
+++ Taledynamic.Bot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+
+namespace TaleDynamicBot
+{
+    public static class Program 
+    {
+        public static async Task Main()
+        {
+            var botClient = new TelegramBotClient(Configuration.BotToken);
+
+            var me = await botClient.GetMeAsync();
+
+            using var cts = new CancellationTokenSource();
+            
+            botClient.StartReceiving(
+                new DefaultUpdateHandler(Handlers.HandleUpdateAsync, Handlers.HandleErrorAsync),
+                cts.Token);
+
+
+            
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+            
+            cts.Cancel();
+        }
+    }
+}
