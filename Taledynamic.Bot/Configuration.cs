2a3d9446d3d918c3459c0dbecea0ada1b3fd495a
Creator148 <bak-home@mail.ru>
2021-10-17 22:40:15 +0300

    Add log(serilog)

--- Taledynamic.Bot/Configuration.cs
+++ Taledynamic.Bot/Configuration.cs
@@ -4,6 +4,7 @@ namespace TaleDynamicBot
 {
     public static class Configuration
     {
-        public readonly static string BotToken = Environment.GetEnvironmentVariable("botoken");
+        public readonly static string BotToken = "2048730205:AAETOGCcu6ONs9UBP1Bu1uV5tLhEvyEozGc";
     }
+
 }
