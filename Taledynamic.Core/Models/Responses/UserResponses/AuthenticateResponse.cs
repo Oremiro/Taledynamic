35fcc410751bdd2ca58e7ea8913869f477e003ba
Yaroslav <bondarev.yaroslav@outlook.com>
2021-10-19 00:06:41 +0300

    task-7: fixes in models, add more checks in service

--- Taledynamic.Core/Models/Responses/UserResponses/AuthenticateResponse.cs
+++ Taledynamic.Core/Models/Responses/UserResponses/AuthenticateResponse.cs
@@ -9,15 +9,6 @@ namespace Taledynamic.Core.Models.Responses.UserResponses
         public string Email { get; set; }
         public string JwtToken { get; set; }
 
-        [JsonIgnore]
-        public string RefreshToken { get; set; }
-
-        public AuthenticateResponse(User user, string jwtToken, string refreshToken)
-        {
-            Id = user.Id;
-            Email = user.Email;
-            JwtToken = jwtToken;
-            RefreshToken = refreshToken;
-        }
+        [JsonIgnore] public string RefreshToken { get; set; }
     }
 }
