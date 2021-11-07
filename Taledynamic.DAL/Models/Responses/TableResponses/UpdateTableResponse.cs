8aa05462ef7687893a077a0b1aa4f75641c8a0f4
Yaroslav <bondarev.yaroslav@outlook.com>
2021-11-07 16:02:43 +0300

    task-41: add workspaces dto and table dto in create/update response

--- Taledynamic.DAL/Models/Responses/TableResponses/UpdateTableResponse.cs
+++ Taledynamic.DAL/Models/Responses/TableResponses/UpdateTableResponse.cs
@@ -1,7 +1,9 @@
+using Taledynamic.DAL.Models.DTOs;
+
 namespace Taledynamic.DAL.Models.Responses.TableResponses
 {
     public class UpdateTableResponse: BaseResponse
     {
-        
+        public TableDto Table { get; set; }
     }
 }
