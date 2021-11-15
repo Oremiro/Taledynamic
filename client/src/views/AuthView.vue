4af0293321f5e5a20fb1d1381c0d43ae0abfead7
1asagne <74735618+1asagne@users.noreply.github.com>
2021-11-15 23:19:26 +0300

    Added prettier

--- client/src/views/AuthView.vue
+++ client/src/views/AuthView.vue
@@ -1,31 +1,16 @@
 <template>
   <div class="container">
-    <n-card
-      hoverable
-      style="max-width: 30rem;"
-    >
+    <n-card hoverable style="max-width: 30rem">
       <n-tabs
         v-model:value="activeTab"
         size="large"
         @update:value="handleUpdateTab"
       >
-        <n-tab-pane
-          name="signin"
-          tab="Вход"
-        />
-        <n-tab-pane
-          name="signup"
-          tab="Регистрация"
-        />
+        <n-tab-pane name="signin" tab="Вход" />
+        <n-tab-pane name="signup" tab="Регистрация" />
       </n-tabs>
-      <router-view
-        v-slot="{ Component }"
-        style="padding-top: 1rem"
-      >
-        <transition
-          name="fade"
-          mode="out-in"
-        >
+      <router-view v-slot="{ Component }" style="padding-top: 1rem">
+        <transition name="fade" mode="out-in">
           <component :is="Component" />
         </transition>
       </router-view>
@@ -34,16 +19,18 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { onBeforeRouteUpdate, useRouter } from 'vue-router';
+import { ref } from "vue";
+import { onBeforeRouteUpdate, useRouter } from "vue-router";
 
 const router = useRouter();
-const activeTab = ref(router.currentRoute.value.name === 'AuthSignIn' ? 'signin' : 'signup');
+const activeTab = ref(
+  router.currentRoute.value.name === "AuthSignIn" ? "signin" : "signup"
+);
 
 function handleUpdateTab(value: string): void {
-	router.push({ name: value === 'signin' ? 'AuthSignIn' : 'AuthSignUp' })
+  router.push({ name: value === "signin" ? "AuthSignIn" : "AuthSignUp" });
 }
-onBeforeRouteUpdate(to => {
-	activeTab.value = (to.name === 'AuthSignIn' ? 'signin' : 'signup');
+onBeforeRouteUpdate((to) => {
+  activeTab.value = to.name === "AuthSignIn" ? "signin" : "signup";
 });
 </script>
