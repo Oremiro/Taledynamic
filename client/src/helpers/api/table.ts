e91e21b2621dff32934aeaf6d05e9ee88a33c7cf
1asagne <74735618+1asagne@users.noreply.github.com>
2021-11-20 15:00:34 +0300

    Added table api helper and requests/responses intefaces

---
+++ client/src/helpers/api/table.ts
@@ -0,0 +1,82 @@
+import {
+  TableCreateRequest,
+  TableDeleteRequest,
+  TableGetListRequest,
+  TableGetRequest,
+  TableUpdateRequest
+} from "@/interfaces/api/requests";
+import {
+  TableCreateResponse,
+  TableDeleteResponse,
+  TableGetListResponse,
+  TableGetResponse,
+  TableUpdateResponse
+} from "@/interfaces/api/responses";
+import axios, { AxiosPromise } from "axios";
+
+export class TableApi {
+  private static readonly axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASEURL + "/data/table",
+    withCredentials: true
+  });
+
+  static create(
+    data: TableCreateRequest,
+    accessToken: string
+  ): AxiosPromise<TableCreateResponse> {
+    return this.axiosInstance.post<TableCreateResponse>("/create", data, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+  }
+
+  static delete(
+    params: TableDeleteRequest,
+    accessToken: string
+  ): AxiosPromise<TableDeleteResponse> {
+    return this.axiosInstance.delete<TableDeleteResponse>("/delete", {
+      params,
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+  }
+
+  static update(
+    data: TableUpdateRequest,
+    accessToken: string
+  ): AxiosPromise<TableUpdateResponse> {
+    return this.axiosInstance.put<TableUpdateResponse>("/update", data, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+  }
+
+  static get(
+    params: TableGetRequest,
+    accessToken: string
+  ): AxiosPromise<TableGetResponse> {
+    return this.axiosInstance.get<TableGetResponse>("/get", {
+      params,
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+  }
+  static getList(
+    params: TableGetListRequest,
+    accessToken: string
+  ): AxiosPromise<TableGetListResponse> {
+    return this.axiosInstance.get<TableGetListResponse>(
+      "/get-filtered-by-workspace",
+      {
+        params,
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      }
+    );
+  }
+}
--- client/src/interfaces/api/requests/table.ts
+++ client/src/interfaces/api/requests/table.ts
@@ -1,1 +1,22 @@
-export {};
+export interface TableCreateRequest {
+  name: string;
+  workspaceId: number;
+}
+
+export interface TableDeleteRequest {
+  id: number;
+}
+
+export interface TableUpdateRequest {
+  id: number;
+  name: string;
+}
+
+export interface TableGetRequest {
+  id: number;
+  workspaceId: number;
+}
+
+export interface TableGetListRequest {
+  workspaceId: number;
+}
