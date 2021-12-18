8693f42a802a229a33a61a7fede5eb904762c8bb
1asagne <74735618+1asagne@users.noreply.github.com>
2021-12-18 19:50:58 +0300

    Added image displaying when file is uploaded

--- client/src/components/table/TableCellImage.vue
+++ client/src/components/table/TableCellImage.vue
@@ -1,37 +1,74 @@
 <template>
-  <n-image v-if="false" width="25" />
-  <n-upload
-    v-else
-    accept="image/png, image/jpeg"
-    :max="1"
-    list-type="image-card"
-    :custom-request="customRequest"
-    @before-upload="onBeforeUpload"
-  >
-    <n-upload-dragger></n-upload-dragger>
-  </n-upload>
+  <div style="height: 34px">
+    <div v-if="isFileUploaded" style="display: flex; gap: .6rem;">
+      <n-image :height="34" object-fit="contain" :src="fileBase64" />
+      <dynamically-typed-button type="error" text @click="deleteImage">
+        <n-icon size="1rem">
+          <dismiss-icon />
+        </n-icon>
+      </dynamically-typed-button>
+    </div>
+    <n-upload
+      v-else
+      accept="image/png, image/jpeg"
+      :max="1"
+      :custom-request="customRequest"
+      file-list-style="margin: 0"
+      style="height: 100%; position: relative"
+      @before-upload="onBeforeUpload"
+    >
+      <n-upload-dragger
+        style="
+          position: absolute;
+          top: 0;
+          padding: 0;
+          height: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        "
+      >
+        <n-text :depth="3"> Нажмите или перетащите </n-text>
+      </n-upload-dragger>
+    </n-upload>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { NImage, NUpload, NUploadDragger, UploadCustomRequestOptions, UploadFileInfo } from "naive-ui";
+import { ref } from "vue";
+import { NImage, NUpload, NUploadDragger, UploadCustomRequestOptions, UploadFileInfo, useMessage } from "naive-ui";
+import { DismissIcon } from "@/components/icons";
+import DynamicallyTypedButton from "@/components/DynamicallyTypedButton.vue";
 
 interface OnBeforeUploadOptions {
   file: UploadFileInfo;
   fileList: UploadFileInfo[];
 }
 
+const isFileUploaded = ref<boolean>(false);
+const fileBase64 = ref<string>();
+
+const message = useMessage();
+
 async function onBeforeUpload({ file }: OnBeforeUploadOptions) {
   if (file.file?.type !== "image/png" && file.file?.type !== "image/jpeg") {
+    message.error("Тип изображения может быть только jpeg или png")
     return false;
   }
   return true;
 }
 
 async function toBase64(file: File) {
-  return new Promise((resolve, reject) => {
+  return new Promise<string>((resolve, reject) => {
     const fileReader = new FileReader();
     fileReader.readAsDataURL(file);
-    fileReader.onload = () => resolve(fileReader.result);
+    fileReader.onload = () => {
+      if (typeof fileReader.result === "string") {
+        resolve(fileReader.result);
+      } else {
+        reject(new Error("Result isn't base64 string"));
+      }
+    };
     fileReader.onerror = (error) => reject(error);
   });
 }
@@ -41,12 +78,17 @@ async function customRequest({ file, onFinish, onError }: UploadCustomRequestOpt
     onError();
   } else {
     try {
-      const result = await toBase64(file.file);
-      console.log(result);
+      fileBase64.value = await toBase64(file.file);
+      isFileUploaded.value = true;
       onFinish();
     } catch (error) {
       onError();
     }
   }
+}
+
+function deleteImage() {
+  isFileUploaded.value = false;
+  fileBase64.value = "";
 }
 </script>
