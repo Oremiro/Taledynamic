002df8b53c7732d4e1698a193e83117092f4662d
Yaroslav <bondarev.yaroslav@outlook.com>
2021-12-29 22:44:46 +0300

    feature/task-ivan-stuff add sneaky comment about token in repository

--- Taledynamic.Bot/Configuration.cs
+++ Taledynamic.Bot/Configuration.cs
@@ -4,6 +4,7 @@ namespace TaleDynamicBot
 {
     public static class Configuration
     {
+        // хихи
         public readonly static string BotToken = "2048730205:AAHfo3UACpWT4vyrJtfR-wUochgb4xinU-M";
     }
 }
