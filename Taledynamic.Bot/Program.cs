ab2652cd39cfaf4db784c8e01ffadc3c9dbcd6e6
Creator148 <bak-home@mail.ru>
2021-12-30 12:55:57 +0300

    Change Logs x3

--- Taledynamic.Bot/Program.cs
+++ Taledynamic.Bot/Program.cs
@@ -13,9 +13,10 @@ namespace TaleDynamicBot
     public static class Program 
     {
         public static async Task Main()
-        {
-            string NameDir_of_Logs = "Logs.bot";
-            string fullpath = Path.GetFullPath(NameDir_of_Logs);
+        { 
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string fullPath = Directory.GetParent(baseDirectory)?.Parent?.Parent?.Parent?.FullName;
+          Console.WriteLine(fullPath);
 
             var botClient = new TelegramBotClient(Configuration.BotToken);
 
@@ -27,7 +28,7 @@ namespace TaleDynamicBot
             
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console()
-                .WriteTo.File(fullpath + @"\Logs.txt",rollingInterval: RollingInterval.Day)
+                .WriteTo.File(fullPath + @"\Logs\Logs.txt",rollingInterval: RollingInterval.Day)
                 .CreateLogger();
             
             ReceiverOptions receiverOptions = new()
